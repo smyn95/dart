5b697800a1e424d096c52408e41fea4dc646aa96
smyn95 <tlsalduszz@gmail.com>
2024-05-05 16:33:34 +0900

    optional positional parameter

--- main.dart
+++ main.dart
@@ -1,26 +1,8 @@
-String sayHello(
-    {required String name, required int age, required String country}) {
+String sayHello(String name, int age, [String? country = 'korea']) {
   return "${name} / ${age} / ${country}";
 }
 
 void main() {
-  String? name = "smyn";
-  name = null;
-  name?.isNotEmpty;
-
-  late String name2;
-  name2 = "smyn";
-
-  final String name3 = "smyn";
-  // name3 = "smyn3"; // Error
-
-  var oldFriends = ["nico", "lynn"];
-  var newFriends = [
-    "tom",
-    "jon",
-    for (var friend in oldFriends) "❤️ $friend",
-  ];
-
-  print(newFriends);
-  print(sayHello(name: "sugar", age: 10, country: "Korea"));
+  var results = sayHello("sugar", 28);
+  print(results);
 }
